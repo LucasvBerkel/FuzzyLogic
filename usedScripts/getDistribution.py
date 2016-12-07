aca11c22fa900d46b0e37e47d49d0d8ab223e3db
Lucas <lucasvberkel@gmail.com>
2016-12-07 10:55:51 +0100

    Made scripts pep8

--- usedScripts/getDistribution.py
+++ usedScripts/getDistribution.py
@@ -8,12 +8,15 @@ from shutil import copyfile
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", help="Copy files, yes or no", type=str)
-parser.add_argument("-t", help="Threshold for amount of reviews per movie", type=int)
+parser.add_argument("-t", help="Threshold for amount " +
+                    " of reviews per movie", type=int)
 args = parser.parse_args()
+
 
 def getpercent(currentline, totallines):
     i = (currentline / totallines) * 100
     return i
+
 
 # Prints the status to stdout
 def writestatus(currentline, totallines):
@@ -22,56 +25,58 @@ def writestatus(currentline, totallines):
     stdout.flush()
 
 if __name__ == "__main__":
-	path = "training_set_without_old_reviews/"
-	newPath = "training_set_without_old_reviews_impopular_movies/"
-	files = os.listdir(path)
+    path = "training_set_without_old_reviews/"
+    newPath = "training_set_without_old_reviews_impopular_movies/"
+    files = os.listdir(path)
 
-	totalLength = len(files)
-	counter = 0
-	vector = []
-	
-	threshold = args.t
+    totalLength = len(files)
+    counter = 0
+    vector = []
 
-	counterUnder = 0
-	counterOver = 0
-	counterTotal = 0
+    threshold = args.t
 
-	counterOverMovies = 0
-	counterUnderMovies = 0
-	for fileName in files:
-		writestatus(counter, totalLength)
-		counter += 1
-		filePath = path + fileName
-		newFilePath = newPath + fileName
-		with open(filePath, 'r') as f:
-			csvFile = csv.reader(f, delimiter=',', quotechar='|')
-			data = list(csvFile)
-    		row_count = len(data)
-    		vector.append(row_count)
-    		if row_count > threshold:
-    			counterOver += row_count
-    			counterOverMovies += 1
-    			if args.c == "yes":
-    				copyfile(filePath, newFilePath)
-    		else:
-    			counterUnder += row_count
-    			counterUnderMovies += 1
-    		counterTotal += row_count
-	
-	array = np.asarray(vector)
-	print "\nMaximum: " + str(array.max())
-	print "Minimum: " + str(array.min())
+    counterUnder = 0
+    counterOver = 0
+    counterTotal = 0
 
-	print ("\nNumber of valid reviews: " + str(counterOver))
-	print ("Number of invalid reviews: " + str(counterUnder))
-	print ("Total of reviews: " + str(counterTotal))
+    counterOverMovies = 0
+    counterUnderMovies = 0
 
-	print ("\nNumber of valid movies: " + str(counterOverMovies))
-	print ("Number of invalid movies: " + str(counterUnderMovies))
-	print ("Total of movies: " + str(totalLength))
+    for fileName in files:
+        writestatus(counter, totalLength)
+        counter += 1
+        filePath = path + fileName
+        newFilePath = newPath + fileName
+        with open(filePath, 'r') as f:
+            csvFile = csv.reader(f, delimiter=',', quotechar='|')
+            data = list(csvFile)
+            row_count = len(data)
+            vector.append(row_count)
+            if row_count > threshold:
+                counterOver += row_count
+                counterOverMovies += 1
+                if args.c == "yes":
+                    copyfile(filePath, newFilePath)
+            else:
+                counterUnder += row_count
+                counterUnderMovies += 1
+            counterTotal += row_count
+        f.close()
 
-	plt.hist(array, bins=100)
-	plt.title('Histogram: Number of reviews per movie')
-	plt.xlabel('Number of reviews')
-	plt.ylabel('Number of movies')
-	plt.show()
+    array = np.asarray(vector)
+    print("\nMaximum: " + str(array.max()))
+    print("Minimum: " + str(array.min()))
+
+    print("\nNumber of valid reviews: " + str(counterOver))
+    print("Number of invalid reviews: " + str(counterUnder))
+    print("Total of reviews: " + str(counterTotal))
+
+    print("\nNumber of valid movies: " + str(counterOverMovies))
+    print("Number of invalid movies: " + str(counterUnderMovies))
+    print("Total of movies: " + str(totalLength))
+
+    plt.hist(array, bins=100)
+    plt.title('Histogram: Number of reviews per movie')
+    plt.xlabel('Number of reviews')
+    plt.ylabel('Number of movies')
+    plt.show()
--- usedScripts/getUserDistribution.py
+++ usedScripts/getUserDistribution.py
@@ -7,14 +7,17 @@ import numpy as np
 from shutil import copyfile
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-t", help="Threshold for amount of reviews per user", type=int)
-parser.add_argument("-d", help="Delete user below threshold, yes or no", type=str)
+parser.add_argument("-t", help="Threshold for amount " +
+                    "of reviews per user", type=int)
+parser.add_argument("-d", help="Delete user below threshold," +
+                    " yes or no", type=str)
 args = parser.parse_args()
 
 
 def getpercent(currentline, totallines):
     i = (currentline / totallines) * 100
     return i
+
 
 # Prints the status to stdout
 def writestatus(currentline, totallines):
@@ -23,75 +26,73 @@ def writestatus(currentline, totallines):
     stdout.flush()
 
 if __name__ == "__main__":
-	path = "training_set_without_old_reviews_impopular_movies/"
-	newPath = "training_set_without_old_reviews_impopular_movies_inactive_users/"
-	files = os.listdir(path)
+    path = "training_set_without_old_reviews_impopular_movies/"
+    newPath = ("training_set_without_old_reviews_impopular_movies" +
+               "_inactive_users/")
+    files = os.listdir(path)
 
-	totalLength = len(files)
-	counter = 0
-	user = {}
+    totalLength = len(files)
+    counter = 0
+    user = {}
 
-	print ("Gather user information...")
-	for fileName in files:
-		writestatus(counter, totalLength)
-		counter += 1
-		filePath = path + fileName
-		with open(filePath, 'r') as f:
-			csvFile = csv.reader(f, delimiter=',', quotechar='|')
-			for row in csvFile:
-				if row[0] not in user:
-					user[row[0]] = 1
-				else:
-					user[row[0]] += 1
-		f.close()
+    print("Gather user information...")
+    for fileName in files:
+        writestatus(counter, totalLength)
+        counter += 1
+        filePath = path + fileName
+        with open(filePath, 'r') as f:
+            csvFile = csv.reader(f, delimiter=',', quotechar='|')
+            for row in csvFile:
+                if row[0] not in user:
+                    user[row[0]] = 1
+                else:
+                    user[row[0]] += 1
+        f.close()
 
-	counter = 0
-	numberReviews = []
-	threshold = args.t
-	counterInactive = 0
-	counterActive = 0
-	inactiveUsers = {}
+    counter = 0
+    numberReviews = []
+    threshold = args.t
+    counterInactive = 0
+    counterActive = 0
+    inactiveUsers = {}
 
-	for key in user:
-		numberReviews.append(user[key])
-		if user[key] < threshold:
-			counterInactive += 1
-			inactiveUsers[key] = True
-		else:
-			counterActive += 1
-			inactiveUsers[key] = False
-		counter += 1
+    for key in user:
+        numberReviews.append(user[key])
+        if user[key] < threshold:
+            counterInactive += 1
+            inactiveUsers[key] = True
+        else:
+            counterActive += 1
+            inactiveUsers[key] = False
+        counter += 1
 
-	if args.d == "yes":
-		print ("\nDeleting users from dataset below threshold...")
-		counter = 0
-		for fileName in files:
-			writestatus(counter, totalLength)
-			counter += 1
-			filePath = path + fileName
-			newFilePath = newPath + fileName
-			with open(filePath, 'r') as f:
-				csvFile = csv.reader(f, delimiter=',', quotechar='|')
-				with open(newFilePath, 'w') as w:
-					for row in csvFile:
-						if not inactiveUsers[row[0]]:
-							row[2] = row[2] + "\n"
-							w.write(",".join(row))
-				w.close()
-			f.close()
+    if args.d == "yes":
+        print("\nDeleting users from dataset below threshold...")
+        counter = 0
+        for fileName in files:
+            writestatus(counter, totalLength)
+            counter += 1
+            filePath = path + fileName
+            newFilePath = newPath + fileName
+            with open(filePath, 'r') as f:
+                csvFile = csv.reader(f, delimiter=',', quotechar='|')
+                with open(newFilePath, 'w') as w:
+                    for row in csvFile:
+                        if not inactiveUsers[row[0]]:
+                            row[2] = row[2] + "\n"
+                            w.write(",".join(row))
+                w.close()
+            f.close()
 
+    array = np.asarray(numberReviews)
 
+    print("\nNumber of users: " + str(counter))
+    print("\nThreshold inactive user: " + str(threshold) + " reviews")
+    print("Number of active users: " + str(counterActive))
+    print("Number of inactive users: " + str(counterInactive))
 
-
-	array = np.asarray(numberReviews)
-
-	print("\nNumber of users: " + str(counter))
-	print("\nThreshold inactive user: " + str(threshold) + " reviews")
-	print ("Number of active users: " + str(counterActive))
-	print ("Number of inactive users: " + str(counterInactive))
-	
-	plt.hist(array, bins=100)
-	plt.title('Histogram: Number of reviews per user')
-	plt.xlabel('Number of reviews')
-	plt.ylabel('Number of users')
-	plt.show()
+    plt.hist(array, bins=100)
+    plt.title('Histogram: Number of reviews per user')
+    plt.xlabel('Number of reviews')
+    plt.ylabel('Number of users')
+    plt.show()
