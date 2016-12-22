2c490e2e7cfce091114966b4bab6d935dac872f7
Lucas <lucasvberkel@gmail.com>
2016-12-22 23:06:55 +0100

    Comment

--- submitFolder/getRecomsOnly.py
+++ submitFolder/getRecomsOnly.py
@@ -1,32 +1,36 @@
 import csv
-from pprint import pprint
 import pickle
-import numpy as np
 import argparse
-import operator
 import os
 
 from content_based import mainSolo as content_based
 from collabr_filter import main as collabr_filter
+
+# This method is used to get the top 20 recommendations using the combined method
+# User can give a user id from which the best recommendations are calculated.
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", help="Give userId to " +
                                "calculate best options", type=str)
 args = parser.parse_args()
 
+# Method to load pickle file to python dictionary
 def load_obj(name):
     with open(name + '.pkl', 'rb') as f:
         return pickle.load(f)
 
 if __name__ == "__main__":
+    # Number of recommendations
     N = 20
     user = args.u
 
+    # Path to training set
     path = "./training_set_tiny_part/"
     files = os.listdir(path)
 
     reader = csv.reader(open(path + user + ".txt", 'r'))
 
+    # Filling dicts and array of dicts so collabr_filter and content_based can handle them
     user = {}
     for row in reader:
         movie, rating, niks = row
@@ -42,6 +46,7 @@ if __name__ == "__main__":
               userdic[movie] = int(rating)
         arrayofdics.append(userdic)
 
+    # Loading moviedict(id corresponding to array of membership of genres)
     movieDict = load_obj("movieDict")
     print("Get content-based recommendations...")
     contentDict = content_based(user, movieDict)
@@ -50,6 +55,7 @@ if __name__ == "__main__":
     print("")
     confidenceMovies = []
 
+    # Transport dict to array for sorting purposes
     for key in collabrDict:
         if key in contentDict:
             confidence = float(collabrDict[key])*float(contentDict[key])
