c1f6c88297f624b31c7950b63ea6648da1bd9b42
Lucas <lucasvberkel@gmail.com>
2016-12-22 23:09:13 +0100

    Moved comments

--- submitFolder/collabr_filter.py
+++ submitFolder/collabr_filter.py
@@ -7,14 +7,14 @@ import operator
 import pandas
 import pickle
 
-#This function applies the collaborative filter algorithm.  It uses the data of an input user and the data of all other users to create
-#a list of recommendation values for each of the movies seen by the K nearest neighbours of the input user.
+# This function applies the collaborative filter algorithm.  It uses the data of an input user and the data of all other users to create
+# a list of recommendation values for each of the movies seen by the K nearest neighbours of the input user.
 
 def load_obj(name):
     with open(name + '.pkl', 'rb') as f:
         return pickle.load(f)
 
-#Reads the data of a user and puts it in a dictionary.
+# Reads the data of a user and puts it in a dictionary.
 
 def readInputUser(fileName):
 	reader = csv.reader(open(fileName, 'r'))
@@ -24,8 +24,8 @@ def readInputUser(fileName):
 	   user[movie] = rating
 	return user
  
-#Reads all the data in the given path and puts that in a List of dictionaries, 
-#each dictionary represents one user and contains its ratings and corresponding movie id's 
+# Reads all the data in the given path and puts that in a List of dictionaries, 
+# each dictionary represents one user and contains its ratings and corresponding movie id's 
 
 def readAllUsers(path):
 	files = os.listdir(path)
@@ -40,8 +40,8 @@ def readAllUsers(path):
 		arrayofdics.append(userdic)
 	return arrayofdics
 
-#Compares each user to the input user to see if they have rated a least number of movies
-#which the input user also rated and stores them in a new array.
+# Compares each user to the input user to see if they have rated a least number of movies
+# which the input user also rated and stores them in a new array.
 
 def getAllValids(user, arrayofdics, leastNrOfSameMovies):
 	valids = 0
@@ -59,9 +59,9 @@ def getAllValids(user, arrayofdics, leastNrOfSameMovies):
 	
 	return(arrayofvalid)
 
-#To compare users to each other two vectors need to be made representing the ratings for the movies
-#that both users watched. This function finds the movies that both the user and all other users watched
-#and returns to vectors which indices represents each movie
+# To compare users to each other two vectors need to be made representing the ratings for the movies
+# that both users watched. This function finds the movies that both the user and all other users watched
+# and returns to vectors which indices represents each movie
 
 def getRatingVectors(user, arrayofvalid):
 
@@ -79,14 +79,14 @@ def getRatingVectors(user, arrayofvalid):
 		AlluserRatings.append(np.asarray(userRatings))
 		AllOtheruserRatings.append(np.asarray(otherUserRatings))
 	
-	#These two lines can be uncommented to get a better understanding of what is happening here
-	#print('for the input user the ratings were ', AlluserRatings[0])
-	#print('for the first other user the ratings were ', AllOtheruserRatings[0])
+	# These two lines can be uncommented to get a better understanding of what is happening here
+	# print('for the input user the ratings were ', AlluserRatings[0])
+	# print('for the first other user the ratings were ', AllOtheruserRatings[0])
 	
 	return AlluserRatings, AllOtheruserRatings
 
-#This function calculates the distance between two vectors and returns the K closest ones in an array
-#The first element in the output list is ignored because that is always the input user itself with 0 distance
+# This function calculates the distance between two vectors and returns the K closest ones in an array
+# The first element in the output list is ignored because that is always the input user itself with 0 distance
 
 def getKMostsimilarUsers(arrayofvalid, AlluserRatings, AllOtheruserRatings, K):
 	i = 0
@@ -97,14 +97,13 @@ def getKMostsimilarUsers(arrayofvalid, AlluserRatings, AllOtheruserRatings, K):
 	arrayofvalid.sort(key=operator.itemgetter('distance'))
 	return arrayofvalid[1:K+1]
 
-#
-#This function scrolls over all the movies that the K most similar users have watched and gives each of them
-#a weighed rating, this rating is based on the average rating for that movie, how many times it has been rated and the average
-#rating of all the movies in the database. This returns a true Bayesian estimate.
+# This function scrolls over all the movies that the K most similar users have watched and gives each of them
+# a weighed rating, this rating is based on the average rating for that movie, how many times it has been rated and the average
+# rating of all the movies in the database. This returns a true Bayesian estimate.
 
 def computeWeighedRatings( KMostsimilarUsers, m):
 
-	#First the K nearest neighbours given a weight based on their distance in order to fuzzify the KNN algorithm
+	# First the K nearest neighbours given a weight based on their distance in order to fuzzify the KNN algorithm
 	
 	# Set minimum distance
 	minDistance = KMostsimilarUsers[0]['distance']
@@ -131,7 +130,7 @@ def computeWeighedRatings( KMostsimilarUsers, m):
 				if totalFuzzyDistance > 0:
 					meanratings[key] = (float(totalRating)/float(totalFuzzyDistance))
 				else: 
-					#should not happen but prevents division by 0
+					# Should not happen but prevents division by 0
 					meanratings[key] = 0
 
 				
@@ -145,7 +144,7 @@ def computeWeighedRatings( KMostsimilarUsers, m):
 		weighedratings[movie] = weighedRating
 	return weighedratings
 
-#This function returns the weighed ratings of the movies that the input user has not already seen.
+# This function returns the weighed ratings of the movies that the input user has not already seen.
 
 def getTopReccomendations(weighedRatings, user):
 	sortedWeighedRatings = sorted(weighedRatings, key=weighedRatings.get, reverse = True)
@@ -163,12 +162,12 @@ def main(user, arrayofdics):
 
     filePath = path + str(user) + ".txt"
     
-    #The percentage of movies that another user must have seen in the list of movies of the input user
+    # The percentage of movies that another user must have seen in the list of movies of the input user
     PercentageOfSameMovies = 0.6 
 
-    #The K nearest neighbours are considered as the similar users which are used to base the recommendation on
+    # The K nearest neighbours are considered as the similar users which are used to base the recommendation on
     K =  7
-    #The least number of ratings needed for a movie to be considered for a high rating in the weighed ratings (note: the maximum is K)
+    # The least number of ratings needed for a movie to be considered for a high rating in the weighed ratings (note: the maximum is K)
     m = 0.5 * K 
 	   
     leastNrOfSameMovies = math.floor(PercentageOfSameMovies * len(user))
