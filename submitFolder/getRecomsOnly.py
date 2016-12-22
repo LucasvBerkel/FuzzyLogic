bbbc6481aa9fccdbf49a04c81b41ce7e73c2139a
Lucas <lucasvberkel@gmail.com>
2016-12-22 22:15:31 +0100

    Added getRecomsOnly for 20 best recommendations

--- submitFolder/getRecomsOnly.py
+++ submitFolder/getRecomsOnly.py
@@ -4,13 +4,14 @@ import pickle
 import numpy as np
 import argparse
 import operator
+import os
 
-from content_based import main as content_based
+from content_based import mainSolo as content_based
 from collabr_filter import main as collabr_filter
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", help="Give userId to " +
-                               "calculate best options", type=int)
+                               "calculate best options", type=str)
 args = parser.parse_args()
 
 def load_obj(name):
@@ -20,10 +21,32 @@ def load_obj(name):
 if __name__ == "__main__":
     N = 20
     user = args.u
+
+    path = "./training_set_tiny_part/"
+    files = os.listdir(path)
+
+    reader = csv.reader(open(path + user + ".txt", 'r'))
+
+    user = {}
+    for row in reader:
+        movie, rating, niks = row
+        user[movie] = rating
+
+    arrayofdics = []
+    for filename in files:
+        filePath = path + filename
+        reader = csv.reader(open(filePath, 'r'))
+        userdic = {}
+        for row in reader:
+              movie, rating, niks = row
+              userdic[movie] = int(rating)
+        arrayofdics.append(userdic)
+
+    movieDict = load_obj("movieDict")
     print("Get content-based recommendations...")
-    contentDict = content_based(user)
+    contentDict = content_based(user, movieDict)
     print("Get collaborative filtering recommendations...")
-    collabrDict = collabr_filter(user)
+    collabrDict = collabr_filter(user, arrayofdics)
     print("")
     confidenceMovies = []
 
